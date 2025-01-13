0c7885bf218ab18ac7360dcd32e9b6b11d9474f4
Gabriella Calvano <gabcalvano@gmail.com>
2025-01-13 17:55:11 +0100

    completed search function

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,24 @@
 'use strict';
 
 const ulList = document.querySelector('.js-list');
+const inputSearch = document.querySelector('.js-search');
+let palettes;
 
 function renderPalettes (list) {
+    ulList.innerHTML = '';
     for (const palette of list) {
-        let content = `<h3>${palette.name}</h3>`;
         let colors = '';
         for(const color of palette.colors) {
             colors += `<div class="palette__color" style="background-color:#${color}"></div>`
         }
 
-        ulList.innerHTML += `<li>
-        ${content}
-        <div class="palettes">
-        ${colors}
-        </div>
-        </li>`
+        ulList.innerHTML += `
+        <li>
+           <h3>${palette.name}</h3>
+           <div class="palettes">
+               ${colors}
+           </div>
+        </li>`;
     }
 }
 
@@ -23,9 +26,19 @@ function getDataApi () {
     fetch('https://beta.adalab.es/ejercicios-de-los-materiales/js-ejercicio-de-paletas/data/palettes.json')
     .then(resp => resp.json())
     .then(data => {
-        const palettes = data.palettes;
+        palettes = data.palettes;
         renderPalettes(palettes);
     });
 }
 
 getDataApi();
+
+//function filter
+
+function handleSearch (ev) {
+    const valueSearch = ev.target.value;
+    const filteredPalettes = palettes.filter((item)=> item.name.toLowerCase().includes(valueSearch.toLowerCase()));
+    renderPalettes(filteredPalettes);
+}
+
+inputSearch.addEventListener('input', handleSearch);
