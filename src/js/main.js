c9bca780a16e7e936da702263356ba8aaf05b40f
Gabriella Calvano <gabcalvano@gmail.com>
2025-01-13 17:24:37 +0100

    created function getDataApi and renderPalettes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('>> Ready :)');
+const ulList = document.querySelector('.js-list');
+
+function renderPalettes (list) {
+    for (const palette of list) {
+        let content = `<h3>${palette.name}</h3>`;
+        let colors = '';
+        for(const color of palette.colors) {
+            colors += `<div class="palette__color" style="background-color:#${color}"></div>`
+        }
+
+        ulList.innerHTML += `<li>
+        ${content}
+        <div class="palettes">
+        ${colors}
+        </div>
+        </li>`
+    }
+}
+
+function getDataApi () {
+    fetch('https://beta.adalab.es/ejercicios-de-los-materiales/js-ejercicio-de-paletas/data/palettes.json')
+    .then(resp => resp.json())
+    .then(data => {
+        const palettes = data.palettes;
+        renderPalettes(palettes);
+    });
+}
+
+getDataApi();
