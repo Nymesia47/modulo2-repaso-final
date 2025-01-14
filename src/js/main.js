bbb80f70232265a54cb459159ca6cd8b316a930b
Gabriella Calvano <gabcalvano@gmail.com>
2025-01-14 12:58:16 +0100

    class notes part 2

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,40 @@
 
 const ulList = document.querySelector('.js-list');
 const inputSearch = document.querySelector('.js-search');
-let palettes;
+let palettes = [];
+let favPalettes = [];
+
+const handleClickFav = (ev) => {
+    const liClicked = ev.currentTarget.id;
+
+    //buscar la paleta clickada a partir del id
+    const paletteSelect = palettes.find((eachPalette)=>eachPalette.id === liClicked);
+
+    //comprobar que la paleta seleccionada no este en el array de fav - findIndex, find, filter, bucle
+    const indexFavSelected = favPalettes.findIndex((palette) => palette.id === liClicked);
+    if (indexFavSelected  === -1) {
+        favPalettes.push(paletteSelect);
+    } else {};
+    localStorage.setItem('favPalettes', JSON.stringify(favPalettes));
+
+    //eliminar del array
+    //favPalettes.splice(indexFavSelected, 1)
+
+    console.log(favPalettes);
+    renderPalettes(palettes);
+
+
+    
+};
+
+const listenerPalettes = ()=>{
+    const allPalettesLi = document.querySelectorAll ('.js_palette');
+    for (const li of allPalettesLi) {
+        li.addEventListener('click', handleClickFav);
+    }
+};
+
+//pintar paletas
 
 function renderPalettes (list) {
     ulList.innerHTML = '';
@@ -11,27 +44,35 @@ function renderPalettes (list) {
         for(const color of palette.colors) {
             colors += `<div class="palette__color" style="background-color:#${color}"></div>`
         }
+        //verificar si la palette esta en el array de fav
+        const findFav = favPalettes.find(palFav => palFav.id === palette.id);
+        let css = findFav ? 'favorite' : '';
 
         ulList.innerHTML += `
-        <li>
+        <li id= '${ palette.id}' class='js_palette ${css}'>
            <h3>${palette.name}</h3>
            <div class="palettes">
                ${colors}
            </div>
         </li>`;
     }
+    listenerPalettes();
+
 }
+
+//buscar los datos en la API
 
 function getDataApi () {
     fetch('https://beta.adalab.es/ejercicios-de-los-materiales/js-ejercicio-de-paletas/data/palettes.json')
     .then(resp => resp.json())
     .then(data => {
         palettes = data.palettes;
+        console.log(palettes);
         renderPalettes(palettes);
+        //guardar datos en el local storage
+        localStorage.setItem('palettesServer', JSON.stringify(palettes));
     });
 }
-
-getDataApi();
 
 //function filter
 
@@ -42,3 +83,19 @@ function handleSearch (ev) {
 }
 
 inputSearch.addEventListener('input', handleSearch);
+
+//obtengo los datos del LS para saber si ejecuto o no el getDataApi
+
+const dataPalettesLS = localStorage.getItem('palettesServer');
+const favdataPalettesLS = localStorage.getItem('favPalettes');
+if (dataPalettesLS) {
+    palettes = JSON.parse(dataPalettesLS);
+    if(favdataPalettesLS) {
+        favPalettes = JSON.parse(favdataPalettesLS);
+    }
+    renderPalettes(palettes);
+} else {
+    getDataApi();
+}
+
+
